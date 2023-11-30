9dde47d5db7c85887fd1d56b9f18777800e27dea
Denys Pavlov <me@denys.me>
2023-11-29 21:22:53 -0500

    feat: add gmaps place autocomplete for lookups

--- islands/Locator.tsx
+++ islands/Locator.tsx
@@ -1,8 +1,19 @@
+import { Head, IS_BROWSER } from "$fresh/runtime.ts";
 import { type Signal, useSignal } from "@preact/signals";
 import { Button } from "../components/Button.tsx";
 
+const GMAPS_API_KEY = IS_BROWSER ? "" : Deno.env.get("GMAPS_API_KEY");
+
 interface Props {
   action: string;
+}
+
+declare global {
+  interface Window {
+    initMap: () => void;
+    // deno-lint-ignore no-explicit-any
+    google: any; // google API object
+  }
 }
 
 export default function Locator({ action }: Props) {
@@ -32,8 +43,43 @@ export default function Locator({ action }: Props) {
     (e.target as HTMLFormElement).form.submit();
   }
 
+  if (IS_BROWSER) {
+    window.initMap = () => {
+      const { Autocomplete } = window.google?.maps?.places || {};
+
+      const autocomplete = new Autocomplete(
+        document.getElementsByName("address")[0],
+        {
+          fields: ["address_components", "geometry", "name"],
+          types: ["address"],
+        },
+      );
+
+      autocomplete.addListener("place_changed", () => {
+        const place = autocomplete.getPlace();
+        console.dir(place);
+        lat.value = place.geometry?.location.lat();
+        lon.value = place.geometry?.location.lng();
+      });
+    };
+  }
+
   return (
     <form action={action} method="GET">
+      {!IS_BROWSER && (
+        <Head>
+          <script
+            async
+            defer
+            src={`https://maps.googleapis.com/maps/api/js?key=${GMAPS_API_KEY}&libraries=places&callback=initMap`}
+          />
+        </Head>
+      )}
+      <div>
+        <label htmlFor="address">Address:</label>
+        <br />
+        <input type="text" name="address" />
+      </div>
       <div>
         <label htmlFor="lat">Latitude:</label>
         <br />
