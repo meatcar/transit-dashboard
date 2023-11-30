48c84f75e15878e92a3b9428229ca53ec7feaffd
Denys Pavlov <me@denys.me>
2023-11-29 21:20:57 -0500

    refactor: :wastebasket: clean up unused routes

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -5,28 +5,26 @@
 import * as $0 from "./routes/_404.tsx";
 import * as $1 from "./routes/_app.tsx";
 import * as $2 from "./routes/_layout.tsx";
-import * as $3 from "./routes/api/joke.ts";
-import * as $4 from "./routes/greet/[name].tsx";
-import * as $5 from "./routes/index.tsx";
-import * as $6 from "./routes/routes.tsx";
-import * as $7 from "./routes/stops.tsx";
-import * as $$0 from "./islands/Locator.tsx";
-import * as $$1 from "./islands/Slider.tsx";
+import * as $3 from "./routes/index.tsx";
+import * as $4 from "./routes/routes.tsx";
+import * as $5 from "./routes/stops.tsx";
+import * as $$0 from "./islands/Clock.tsx";
+import * as $$1 from "./islands/Locator.tsx";
+import * as $$2 from "./islands/Slider.tsx";
 
 const manifest = {
   routes: {
     "./routes/_404.tsx": $0,
     "./routes/_app.tsx": $1,
     "./routes/_layout.tsx": $2,
-    "./routes/api/joke.ts": $3,
-    "./routes/greet/[name].tsx": $4,
-    "./routes/index.tsx": $5,
-    "./routes/routes.tsx": $6,
-    "./routes/stops.tsx": $7,
+    "./routes/index.tsx": $3,
+    "./routes/routes.tsx": $4,
+    "./routes/stops.tsx": $5,
   },
   islands: {
-    "./islands/Locator.tsx": $$0,
-    "./islands/Slider.tsx": $$1,
+    "./islands/Clock.tsx": $$0,
+    "./islands/Locator.tsx": $$1,
+    "./islands/Slider.tsx": $$2,
   },
   baseUrl: import.meta.url,
 };
