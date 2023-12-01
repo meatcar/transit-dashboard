10889b9bd9a4158e93163291f5790cbec4d9e621
Denys Pavlov <me@denys.me>
2023-12-01 17:51:14 -0500

    feat: add toggle island to dynamicaly hide elements

---
+++ islands/Toggle.tsx
@@ -0,0 +1,42 @@
+import { type Signal, useSignal, useSignalEffect } from "@preact/signals";
+import type { JSX } from "preact/jsx-runtime";
+
+type child = JSX.Element | string | null;
+type ToggleProps =
+  & {
+    children: child | child[];
+    control?: boolean;
+  }
+  & (
+    | { show: Signal<boolean>; hide?: never }
+    | { show?: never; hide: Signal<boolean> }
+  );
+export default function Toggle(props: ToggleProps) {
+  const { children, control } = props;
+  let toggle: Signal<boolean>;
+  if (typeof props.show !== "undefined") toggle = props.show;
+  else {
+    toggle = useSignal(!props.hide.value);
+    useSignalEffect(() => {
+      props.hide.value = !toggle.value;
+    });
+  }
+
+  const onClick = (e: MouseEvent) => {
+    console.log("click", e.target);
+    if ((e.target as HTMLElement).classList.contains("toggle-control")) {
+      toggle.value = !toggle.value;
+    }
+  };
+
+  return (
+    <span
+      class="toggle"
+      onClick={onClick}
+      data-toggle={toggle}
+      data-show={toggle.value}
+    >
+      {children}
+    </span>
+  );
+}
