8c30eb4e9db5a2ef5b4f326766892053fed41940
Denys Pavlov <me@denys.me>
2023-12-18 18:46:24 -0500

    feat: package as a deno app with nix-deno

--- flake.nix
+++ flake.nix
@@ -4,22 +4,53 @@
   inputs = {
     flake-parts.url = "github:hercules-ci/flake-parts";
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nix-deno.url = "github:nekowinston/nix-deno";
   };
 
-  outputs = inputs@{ self, ... }:
-    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
-      perSystem = { pkgs, inputs', ... }:
-        let
-          name = "transit-dashboard";
-        in
-        {
-          devShells.default = pkgs.mkShell {
-            inherit name;
-            buildInputs = with pkgs; [
-              deno
-            ];
-          };
+  outputs = inputs @ {self, ...}:
+    inputs.flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = ["x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin"];
+      perSystem = {
+        system,
+        pkgs,
+        ...
+      }: let
+        name = "transit-dashboard";
+      in {
+        _module.args.pkgs = import inputs.nixpkgs {
+          inherit system;
+          overlays = [inputs.nix-deno.overlays.default];
         };
+        devShells.default = pkgs.mkShell {
+          inherit name;
+          buildInputs = with pkgs; [
+            deno
+            nvfetcher
+          ];
+          shellHook = ''
+            deno task
+          '';
+        };
+        packages.default = let
+          nvfetcher = pkgs.callPackage ./_sources/generated.nix {};
+          esbuild = nvfetcher."esbuild-${pkgs.hostPlatform.system}";
+        in
+          pkgs.denoPlatform.mkDenoDerivation {
+            inherit name;
+            version = "0.1.0";
+
+            src = ./.;
+
+            env.ESBUILD_BINARY_PATH = "${esbuild.src}/bin/esbuild";
+
+            buildPhase = ''
+              deno task build
+            '';
+
+            installPhase = ''
+              cp -r _fresh $out
+            '';
+          };
+      };
     };
 }
---
+++ nix-cache.ts
@@ -0,0 +1,3 @@
+import {} from "https://deno.land/x/fresh@1.6.1/src/runtime/entrypoints/deserializer.ts";
+import {} from "https://deno.land/x/fresh@1.6.1/src/runtime/entrypoints/main.ts";
+import {} from "https://deno.land/x/fresh@1.6.1/src/runtime/entrypoints/signals.ts";
